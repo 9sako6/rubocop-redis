475177164d08ae8f070b23669f8b47fe8fe02e43
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-09-09 12:52:39 +0900

    Add detectable patterns for `Redis/NotKeys`

--- lib/rubocop/cop/redis/not_keys.rb
+++ lib/rubocop/cop/redis/not_keys.rb
@@ -7,15 +7,20 @@ module RuboCop
       # Consider using `SCAN` query instead of `KEYS` query.
       # See https://redis.io/commands/keys/ for details.
       #
+      # To avoid detecting `Hash#keys` calling, this cop adds an offense only if a receiver of `keys` method is named `redis`.
+      #
       # @example
       #   # bad
-      #   Redis.current.keys("pattern-*")
+      #   redis.keys("pattern-*")
+      #
+      #   # bad
+      #   Redis.new.keys("pattern-*")
       #
       #   # good
       #   cursor = 0
       #   all_keys = []
       #   loop do
-      #     cursor, keys = Redis.current.scan(cursor, match: "pattern-*")
+      #     cursor, keys = redis.scan(cursor, match: "pattern-*")
       #     all_keys += keys
       #     break if cursor == "0"
       #   end
@@ -27,15 +32,27 @@ module RuboCop
         def_node_matcher :keys_call?, <<~PATTERN
           (send
             (send
-              (const {cbase nil?} :Redis) :current) :keys $...)
+              (const {cbase nil?} :Redis) {:new | :current} ...) :keys ...)
+        PATTERN
+
+        def_node_matcher :keys_call_for_object?, <<~PATTERN
+          (send ($...) :keys ...)
         PATTERN
 
         RESTRICT_ON_SEND = %i[keys].freeze
 
         def on_send(node)
-          return unless keys_call?(node)
+          return unless keys_call?(node) || keys_call_for_object_named_redis?(node)
 
           add_offense(node)
+        end
+
+        private
+
+        def keys_call_for_object_named_redis?(node)
+          receiver, message = keys_call_for_object?(node)
+
+          receiver.nil? && message == :redis
         end
       end
     end
